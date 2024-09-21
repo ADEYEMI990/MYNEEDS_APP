bcd822812e1278c7a096b101b9aec9e386334c72
Adeyemi <adeyemiibrahim990@gmail.com>
2024-09-21 11:08:50 -0700

    Creating The Product Sort Section

--- MYNEEDS_frontend/src/pages/Collection.jsx
+++ MYNEEDS_frontend/src/pages/Collection.jsx
@@ -4,6 +4,7 @@ import { assets } from '../assets/assets';
 import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
+
 const Collection = () => {
 
   const { products } = useContext(ShopContext);
@@ -11,6 +12,7 @@ const Collection = () => {
   const [filterProducts,setFilterProducts] =useState([]);
   const [category,setCategory] = useState([]);
   const [subCategory, setSubCategory]=useState([]);
+  const [sortType,setSortType] = useState('relevant');
 
   const toggleCategory = (e) => {
     const value = e.target.value;
@@ -45,6 +47,24 @@ const Collection = () => {
     setFilterProducts(productCopy)
   }
 
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+        break;
+
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)))
+        break;
+    
+      default:
+        applyFilter()
+        break;
+    }
+  }
+
   // useEffect(()=> {
   //   setFilterProducts(products);
   // },[])   it should be apply before settling up the applyFilter function
@@ -52,6 +72,10 @@ const Collection = () => {
   useEffect(()=> {
     applyFilter();
   },[category,subCategory])
+
+  useEffect(()=> {
+    sortProduct();
+  },[sortType])
 
   return (
     <div className= 'flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -96,7 +120,7 @@ const Collection = () => {
         <div className= 'flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTION'} />
           {/* product Sort */}
-          <select className= 'border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e)=>setSortType(e.target.value)} className= 'border-2 border-gray-300 text-sm px-2'>
             <option value="relavant">Sort by: Relevant</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
